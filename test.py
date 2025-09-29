4679ca863a2c1e134c6987a2fc88511092d71f21
Daniel Desktop <danieladiazop@gmail.com>
2025-09-28 22:56:08 -0700

    working on task4

---
+++ test.py
@@ -0,0 +1,15 @@
+from dash import Dash, dcc, html, Input, Output # Dash components you need
+import plotly.express as px # Dash relies on Plotly to actually do the plotting.  Plotly creates an HTML page with lots of JavaScript.
+import plotly.data as pldata # This is only needed to give access to the Plotly built in datasets.
+
+df = pldata.stocks(return_type='pandas', indexed=False, datetimes=True) # This loads one of the datasets
+
+#print(df)
+
+
+from dash import Dash, dcc, html, Input, Output # Dash components you need
+import plotly.express as px # Dash relies on Plotly to actually do the plotting.  Plotly creates an HTML page with lots of JavaScript.
+import plotly.data as pldata # This is only needed to give access to the Plotly built in datasets.
+
+df = px.data.gapminder(return_type='pandas')
+print(df)
